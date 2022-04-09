295ae2de2a2bd76dfbaa5df350d047b938138ebb
jay <1627158922@qq.com>
2022-04-09 11:35:45 +0800

    1.添加了GUI 2.完善了佛像图片数据集

---
+++ face_match.py
@@ -0,0 +1,57 @@
+# 指定人脸匹配相似佛脸
+import os, dlib, glob, numpy
+import numpy as np
+
+from skimage import io
+from PIL import Image
+
+detector_path = "data/detector_buddhism.svm"
+# 人脸关键点检测器
+predictor_path = "data/shape_predictor_68_face_landmarks.dat"
+# 人脸识别模型、提取特征值
+face_rec_model_path = "data/dlib_face_recognition_resnet_model_v1.dat"
+
+# 加载模型
+detector = dlib.get_frontal_face_detector()
+# detector = dlib.simple_object_detector(detector_path)
+sp = dlib.shape_predictor(predictor_path)
+facerec = dlib.face_recognition_model_v1(face_rec_model_path)
+
+candidate = np.load("data/candidate_buddhism.npy")  # 训练集人物名字
+descriptors = np.load("data/descriptors_buddhism.npy")  # 训练集人物特征列表
+
+def face_similar(imgpath):
+    try:
+        ##    test_path=input('请输入要检测的图片的路径（记得加后缀哦）:')
+        # imgname = "faces/image.jpg"
+        imgname = imgpath
+        img = io.imread(r"examples/johns/John_Salley/000179_02159509.jpg")
+        imgshow = Image.open(imgname)
+        imgshow.show()
+        dets = detector(img, 1)
+    except:
+        print('输入路径有误，请检查！')
+
+    dist = []
+    for k, d in enumerate(dets):
+        shape = sp(img, d)
+        face_descriptor = facerec.compute_face_descriptor(img, shape)
+        d_test = numpy.array(face_descriptor)
+        for i in descriptors:  # 计算距离
+            dist_ = numpy.linalg.norm(i - d_test)
+            dist.append(dist_)
+
+    # 训练集人物和距离组成一个字典
+    c_d = dict(zip(candidate, dist))
+    cd_sorted = sorted(c_d.items(), key=lambda d: d[1])
+    print("识别到的人物最有可能是: ", cd_sorted[0][0], "（结果越小表明越相似）")
+    print(cd_sorted)
+    print(dist)
+    filename = "faces/buddhism/" + cd_sorted[0][0] + ".jpg"
+    print(filename)
+    img = Image.open(filename)
+    img.show()
+    return filename
+
+if __name__ == '__main__':
+    face_similar("examples/johns/John_Salley/000179_02159509.jpg")
--- main.py
+++ main.py
@@ -1,50 +1,11 @@
-# 指定人脸匹配相似佛脸
-import os, dlib, glob, numpy
-import numpy as np
+import sys
+import mainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from skimage import io
-from PIL import Image
-
-# 人脸关键点检测器
-predictor_path = "data/shape_predictor_68_face_landmarks.dat"
-# 人脸识别模型、提取特征值
-face_rec_model_path = "data/dlib_face_recognition_resnet_model_v1.dat"
-
-# 加载模型
-# detector = dlib.get_frontal_face_detector()
-detector = dlib.simple_object_detector("data/detector_buddhism.svm")
-sp = dlib.shape_predictor(predictor_path)
-facerec = dlib.face_recognition_model_v1(face_rec_model_path)
-
-candidate = np.load("data/candidate_buddhism.npy")  # 训练集人物名字
-descriptors = np.load("data/descriptors_buddhism.npy")  # 训练集人物特征列表
-
-try:
-    ##    test_path=input('请输入要检测的图片的路径（记得加后缀哦）:')
-    imgname = "faces/image.jpg"
-    img = io.imread(r"faces/image.jpg")
-    imgshow = Image.open(imgname)
-    imgshow.show()
-    dets = detector(img, 1)
-except:
-    print('输入路径有误，请检查！')
-
-dist = []
-for k, d in enumerate(dets):
-    shape = sp(img, d)
-    face_descriptor = facerec.compute_face_descriptor(img, shape)
-    d_test = numpy.array(face_descriptor)
-    for i in descriptors:  # 计算距离
-        dist_ = numpy.linalg.norm(i - d_test)
-        dist.append(dist_)
-
-# 训练集人物和距离组成一个字典
-c_d = dict(zip(candidate, dist))
-cd_sorted = sorted(c_d.items(), key=lambda d: d[1])
-print("识别到的人物最有可能是: ", cd_sorted[0][0], "（结果越小表明越相似）")
-print(cd_sorted)
-print(dist)
-filename = "faces/buddhism/" + cd_sorted[0][0] + ".jpg"
-print(filename)
-img = Image.open(filename)
-img.show()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)  # 声明应用程序
+    MainWindow = QMainWindow()  # 声明窗口
+    ui = mainWindow.Ui_FaceBuddhaIdentificationSystem()  # 声明UI对话框，即对应的UI的python文件
+    ui.setupUi(MainWindow)  # 将UI对话框对应声明的窗口
+    MainWindow.show()
+    sys.exit(app.exec_())  # 当点击窗口的x时，退出程序
